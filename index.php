26591a6ad188d36476e74bacebe650377f6653d4
Federico Zuffa <federico.zuffa@epfl.ch>
2026-03-09 15:24:29 +0100

    Correction de liens vers style.css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Pokédex</title>
-    <link rel="stylesheet" href="styleClaude.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/svg+xml" href="./Ressources/PokeBall_icon.ico">
 </head>
 <body>
